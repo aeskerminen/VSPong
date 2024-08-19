fe9a586d67a2f35e5bd375bcfa8f9697d48f85a8
aeskerm <kerminenartturi@gmail.com>
2024-08-19 18:27:52 +0300

    implement basic movement

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const vscode = require("vscode");
+const fs = require("fs");
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -6,19 +7,52 @@ const vscode = require("vscode");
 function activate(context) {
   console.log('Congratulations, your extension "vspong" is now active!');
 
-  const disposable = vscode.commands.registerCommand(
-    "pong.Pong",
-    function () {
-      vscode.window.showInformationMessage("Hello World from VSPong!");
+  const disposable = vscode.commands.registerCommand("pong.Pong", function () {
+    vscode.window.showInformationMessage("Hello World from VSPong!");
 
-      const view = vscode.window.createWebviewPanel(
-        "pong",
-        "Pong",
-        vscode.ViewColumn.One,
-        {}
-      );
-    }
-  );
+    const view = vscode.window.createWebviewPanel(
+      "pong",
+      "Pong",
+      vscode.ViewColumn.One,
+      {
+        localResourceRoots: [
+          vscode.Uri.joinPath(context.extensionUri, "resources"),
+        ],
+        enableScripts: true,
+      }
+    );
+
+    const htmlPath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "resources",
+      "game.html"
+    );
+    const htmlSrc = fs.readFileSync(htmlPath.fsPath, "utf-8");
+    view.webview.html = htmlSrc;
+
+    console.log(htmlSrc);
+
+    /*	
+    const updateGameState = () => {
+		view.title = ["Pong!!", "Pong?", "Pong..."][
+        Math.round(Math.random() * 3) - 1
+      ];
+      view.webview.html = htmlSrc;
+    };
+
+    const interval = setInterval(() => {
+      updateGameState();
+    }, 1000);
+
+    view.onDidDispose(
+      () => {
+        clearInterval(interval);
+      },
+      null,
+      context.subscriptions
+    );
+	*/
+  });
 
   context.subscriptions.push(disposable);
 }
